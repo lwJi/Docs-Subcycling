116525d806c1de28ed93963888b57a7eb9ec5475
Liwei Ji <jiliwei.phys@gmail.com>
2023-12-28 22:47:02 -0500

    ipynb: Add Reflection

---
+++ ipynb/misc.jl
@@ -0,0 +1,33 @@
+module Misc
+
+using DelimitedFiles
+using Plots
+
+function plot_error(
+    f_analytical::Function,
+    dirs;
+    nbuf = 2,
+    lev = 1,
+    tmax = 2.0,
+    parent_dir = expanduser("~/docker-workspace/simulations/"),
+)
+    plt = plot()
+    for i = 1:length(dirs)
+        fulldir = parent_dir * dirs[i]
+        tsvs = filter(file -> endswith(file, ".tsv"), readdir(fulldir))
+        for fname in tsvs
+            data = readdlm(joinpath(fulldir, fname), Float64, comments = true)
+            if isapprox(data[1, 2], tmax; rtol = 1e-12)
+                x = data[findall(x -> x == lev, data[:, 3]), 5]
+                f = data[findall(x -> x == lev, data[:, 3]), 6]
+                x_in = x[1+nbuf:length(x)-nbuf]
+                f_in = f[1+nbuf:length(x)-nbuf]
+                error = (f_in - f_analytical.(tmax, x_in))
+                plt = plot!(x_in, error)
+            end
+        end
+    end
+    return (plt)
+end
+
+end
