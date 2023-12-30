cb0497fcbfddef3283f83a9c545fc80cc956ba6a
Liwei Ji <jiliwei.phys@gmail.com>
2023-12-30 14:12:37 -0500

    ipynb: Update Convergence and Reflection

--- ipynb/misc.jl
+++ ipynb/misc.jl
@@ -1,34 +1,25 @@
 module Misc
 
 using DelimitedFiles
-using Plots
 
-function plot_error(
-    parent_dir,
-    dirs,
-    f_analytical::Function,
-    nbuf,
-    lev,
-    tmax;
-    f_error::Function = (err, i) -> err
-)
-    plt = plot()
-    for i = 1:length(dirs)
-        fulldir = parent_dir * dirs[i]
-        tsvs = filter(file -> endswith(file, ".tsv"), readdir(fulldir))
+function load_data(dirs, nbuf, lev, tmax; column=6)
+    dats = []
+    for dir in dirs
+        tsvs = filter(file -> endswith(file, ".tsv"), readdir(dir))
+        x_in = []
+        f_in = []
         for fname in tsvs
-            data = readdlm(joinpath(fulldir, fname), Float64, comments = true)
+            data = readdlm(joinpath(dir, fname), Float64, comments = true)
             if isapprox(data[1, 2], tmax; rtol = 1e-12)
                 x = data[findall(x -> x == lev, data[:, 3]), 5]
-                f = data[findall(x -> x == lev, data[:, 3]), 6]
+                f = data[findall(x -> x == lev, data[:, 3]), column]
                 x_in = x[1+nbuf:length(x)-nbuf]
                 f_in = f[1+nbuf:length(x)-nbuf]
-                error = (f_in - f_analytical.(tmax, x_in))
-                plt = plot!(x_in, f_error(error, i))
             end
         end
+        push!(dats, [x_in, f_in])
     end
-    return (plt)
+    return dats
 end
 
 end
