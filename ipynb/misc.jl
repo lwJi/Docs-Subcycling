cc204cb836f4185cf508643922360a80c31bd323
Liwei Ji <jiliwei.phys@gmail.com>
2023-12-29 22:30:36 -0500

    ipynb: Update Convergence

--- ipynb/misc.jl
+++ ipynb/misc.jl
@@ -4,13 +4,13 @@ using DelimitedFiles
 using Plots
 
 function plot_error(
+    parent_dir,
+    dirs,
     f_analytical::Function,
-    dirs;
-    nbuf = 2,
-    lev = 1,
-    tmax = 2.0,
-    cord = 0,
-    parent_dir = expanduser("~/docker-workspace/simulations/"),
+    nbuf,
+    lev,
+    tmax;
+    f_error::Function = (err, i) -> err
 )
     plt = plot()
     for i = 1:length(dirs)
@@ -24,7 +24,7 @@ function plot_error(
                 x_in = x[1+nbuf:length(x)-nbuf]
                 f_in = f[1+nbuf:length(x)-nbuf]
                 error = (f_in - f_analytical.(tmax, x_in))
-                plt = plot!(x_in, error * (2^cord)^(i-1))
+                plt = plot!(x_in, f_error(error, i))
             end
         end
     end
